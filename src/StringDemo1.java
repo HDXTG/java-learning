3c13b5aec9533f791dd20e9021f26816d0c6c2ac
HDXTG <hdxtg58@gmail.com>
2025-11-28 12:46:47 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/StringDemo1.java
@@ -0,0 +1,8 @@
+
+public class StringDemo1 {
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 ="ABC";
+        System.out.println(str1.equalsIgnoreCase(str2));
+    }
+}
