e9c5b2696390a1ff8e2977378535c72e084c6ec1
HDXTG <hdxtg58@gmail.com>
2025-11-30 20:52:28 +0800

    Merge remote-tracking branch 'origin/master'

--- src/userLogin.java
+++ src/userLogin.java
@@ -5,15 +5,20 @@ public class userLogin {
        Scanner sc=new Scanner(System.in);
        String right_username = "hdxtg";
        String right_password = "123456";
+
+        for (int i = 0; i < 3; i++) {
+
        System.out.println("请输入账户");
        String username =sc.next();
        System.out.println("请输入密码");
        String password =sc.next();
 
-       if (username.equals(right_username) && password.equals(right_password)) {
-           System.out.println("成功登录");
-       }else{
-           System.out.println("账号或密码错误");
+            if (username.equals(right_username) && password.equals(right_password)) {
+                System.out.println("登录成功");
+            }else{
+                System.out.println("账号或密码错误" + "您还剩下" + (2 - i) + "次机会");
+            }
         }
+
     }
 }
